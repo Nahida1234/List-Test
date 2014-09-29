cbfb6250787c15c9abcbe6cb65e20a6d0146fdc6
nahida1234 <nahida_036@yahoo.com>
2014-09-29 03:52:16 -0400

    Simple List

---
+++ practice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(4);
+            list.Add(5);
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+                Console.Read();
+            }
+
+        }
+    }
+}
